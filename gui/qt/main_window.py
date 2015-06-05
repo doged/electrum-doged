bec337d2b11f8a0e06071b0efd31992820c4c1cc
DogecoinDark <justinvalo@yahoo.com>
2015-06-05 05:30:29 -0400

    Update main_window.py

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -123,7 +123,7 @@ class ElectrumWindow(QMainWindow):
         self.create_status_bar()
         self.need_update = threading.Event()
 
-        self.decimal_point = config.get('decimal_point', 8)
+        self.decimal_point = config.get('decimal_point', 6)
         self.num_zeros     = int(config.get('num_zeros',0))
 
         self.completions = QStringListModel()

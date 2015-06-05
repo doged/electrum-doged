bb77854051a1032937f24dbcaf8180574190bc5e
DogecoinDark <justinvalo@yahoo.com>
2015-06-05 01:39:27 -0400

    Update amountedit.py

--- gui/qt/amountedit.py
+++ gui/qt/amountedit.py
@@ -73,12 +73,12 @@ class BTCAmountEdit(AmountEdit):
 
     def _base_unit(self):
         p = self.decimal_point()
-        assert p in [2, 5, 8]
-        if p == 8:
+        assert p in [0, 3, 6]
+        if p == 6:
             return 'DOGED'
-        if p == 5:
+        if p == 3:
             return 'mDOGED'
-        if p == 2:
+        if p == 0:
             return 'bits'
         raise Exception('Unknown base unit')
 
